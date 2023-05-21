07aa5bf1af5bc728e2bb8ceb391ee22c7c8c03fc
goatedkab <becomegoated@gmail.com>
2023-05-21 16:46:44 +0100

    properties as object

--- script.js
+++ script.js
@@ -3,25 +3,13 @@ const swiper = document.querySelector('#swiper');
 const like = document.querySelector('#like');
 const dislike = document.querySelector('#dislike');
 
-// constants
-const urls = [
-    'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    'https://images.unsplash.com/photo-1600585152915-d208bec867a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80',
-    'https://images.unsplash.com/photo-1600585153490-76fb20a32601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    'https://images.unsplash.com/photo-1613553507747-5f8d62ad5904?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    'https://images.unsplash.com/photo-1416331108676-a22ccb276e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80',
-    'https://images.unsplash.com/photo-1600585152220-90363fe7e115?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-];
 
-const addresses = [
-  'your mums house',
-  'your dads house',
-  'your amns house',
-  'your nanas house',
-  'your nanas house',
-  'your nanas house',
-];
-
+const property_cards = [
+  {url:  'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  address: 'your mums house'},
+  {url:  'https://images.unsplash.com/photo-1600585152915-d208bec867a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80',
+  address: 'your dads house'},
+]
 
 
 // variables
@@ -32,8 +20,8 @@ console.log(urls.length)
 // functions
 function appendNewCard() {
   const card = new Card({
-    imageUrl: urls[cardCount % urls.length],
-    address: addresses[cardCount % addresses.length],
+    imageUrl: property_cards[cardCount % urls.length].url,
+    address: property_cards[cardCount % addresses.length].address,
     onDismiss: appendNewCard,
     onLike: () => {
       like.style.animationPlayState = 'running'; //activates the like button
